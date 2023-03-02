f79de5cbf2d1d53bb4e566e40976712ac2d09241
Lane Vance <lanevance92@gmail.com>
2023-03-01 19:24:59 -0700

    update commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let task1 = "Sweep the floor"
+let task2 = "Get groceries"
+let task3 = "Make dinner"
+
+if (task1 === "Make dinner"){
+    console.log("Completed")
+}else{
+    console.log("Incomplete")
+}
+
+if (task2 === "Make dinner"){
+    console.log("Completed")
+}else{
+    console.log("Incomplete")
+}
+
+if (task3 === "Make dinner"){
+    console.log("Completed")
+}else{
+    console.log("Incomplete")
+}
