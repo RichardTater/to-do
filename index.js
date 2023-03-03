47a27b0735438dd9c8e595b740fc6e1044f3c3c8
Lane Vance <lanevance92@gmail.com>
2023-03-02 17:37:47 -0700

    Fixed Bugs

--- index.js
+++ index.js
@@ -2,27 +2,26 @@ let task1 = "Sweep the floor"
 let task2 = "Get groceries"
 let task3 = "Make dinner"
 
-if (task1 === "Make dinner"){
-    console.log("Completed")
-}else{
-    console.log("Incomplete")
-}
+// if (task1 === "Make dinner"){
+//     console.log("Completed")
+// }else{
+//     console.log("Incomplete")
+// }
 
-if (task2 === "Make dinner"){
-    console.log("Completed")
-}else{
-    console.log("Incomplete")
-}
+// if (task2 === "Make dinner"){
+//     console.log("Completed")
+// }else{
+//     console.log("Incomplete")
+// }
 
-if (task3 === "Make dinner"){
-    console.log("Completed")
-}else{
-    console.log("Incomplete")
-}
-//Need help here
-let tasks = [task1, task2, task3]
+// if (task3 === "Make dinner"){
+//     console.log("Completed")
+// }else{
+//     console.log("Incomplete")
+// }
 
 let tasks = [task1, task2, task3]
+
 
 //write a for loop that will print each of the tasks in the tasks array
 //intermediate challenge: implement the above if statement into that for loop
